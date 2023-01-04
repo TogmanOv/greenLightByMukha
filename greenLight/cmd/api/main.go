8a403f06f1c2c6e0687f1c0a45a59d98b0dcedf1
togmanovDev <97113285+TogmanOv@users.noreply.github.com>
2023-01-04 21:46:50 +0600

    Add files via upload

---
+++ greenLight/cmd/api/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"blacklight.forstes.github.com/internal/data"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleTime  string
+	}
+}
+type application struct {
+	config config
+	logger *log.Logger
+	models data.Models
+}
+
+func main() {
+	var cfg config
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	err := godotenv.Load(".env.local")
+	if err != nil {
+		logger.Fatal("Error loading .env file")
+	}
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("DB_DSN"), "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 15, "PostgreSQL max open connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
+	flag.Parse()
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+	logger.Printf("database connection pool established")
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+		models: data.NewModels(db),
+	}
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf("localhost:%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err = srv.ListenAndServe()
+	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*pgxpool.Pool, error) {
+	pgxConfig, err := pgxpool.ParseConfig(cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	pgxConfig.MaxConns = int32(cfg.db.maxOpenConns)
+	pgxConfig.MaxConnIdleTime = duration
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	db, err := pgxpool.NewWithConfig(ctx, pgxConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
